2c8f38f00d02204992cb83808977b77b4ceed31c
Chelsea Becker <cschweig2@gmail.com>
2020-11-17 16:06:20 -0800

    add assets/UI/logic
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,7 +2,23 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import DinoIpsumService from './services/DinoIpsumService.js';
 
-$(document).ready(function () {
-  
+$(document).ready(function() {
+  $("#user-input").submit(function(event) {
+    event.preventDefault();
+    const paragraphNumber = $("#paragraph-number").val();
+    const wordNumber = $("#word-number").val();
+    $("#paragraph-number, #word-number").val("");
+    let promise = DinoIpsumService.makeDinoIpsum(paragraphNumber, wordNumber);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      body.forEach((index) => {
+        const finalOutput = index.join(" ");
+        $("#output").append(finalOutput + "<br><br>");
+      });
+    }, function(error) {
+      $("#output").text(`There was an error: ${error}`);
+    });
+  });
 });
---
+++ src/services/DinoIpsumService.js
@@ -0,0 +1,18 @@
+export default class DinoIpsumService {
+  static makeDinoIpsum(paragraphNumber, wordNumber) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${paragraphNumber}&words=${wordNumber}`;
+      
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
